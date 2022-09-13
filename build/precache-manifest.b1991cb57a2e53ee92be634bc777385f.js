4d9b48ae3bbf6f2454f24e8d35ead57d9fbfb63d
rakhi.kumaribce@gmail.com <rakhi.kumaribce@gmail.com>
2022-09-13 11:02:17 +0100

    build folder

---
+++ build/precache-manifest.b1991cb57a2e53ee92be634bc777385f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8feb6b5ccc9c0a4b9f",
+    "url": "/static/css/main.81224efe.chunk.css"
+  },
+  {
+    "revision": "1e8feb6b5ccc9c0a4b9f",
+    "url": "/static/js/main.1e8feb6b.chunk.js"
+  },
+  {
+    "revision": "34e91bf41f1cd8c8fe83",
+    "url": "/static/js/1.34e91bf4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8946296bfdf73be9b037ffd947b842f3",
+    "url": "/static/media/portfolio1.8946296b.png"
+  },
+  {
+    "revision": "0700fdf23d63fdff234bd572e676457a",
+    "url": "/static/media/resume.0700fdf2.pdf"
+  },
+  {
+    "revision": "d2d36192e22bbd0c7cbd385199f2c4a1",
+    "url": "/static/media/me.d2d36192.jpg"
+  },
+  {
+    "revision": "280d1a0d19e711b7a3b848baa090589f",
+    "url": "/static/media/headerImg.280d1a0d.jpeg"
+  },
+  {
+    "revision": "038920f3527d353168a4190870aef59f",
+    "url": "/index.html"
+  }
+];
